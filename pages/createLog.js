fffbed154946b14aee999fae6d4c8f72fdbf0872
dannguyen8189 <dannguyen8189@gmail.com>
2025-05-22 11:20:55 -0700

    Revert "set env variables for local vs prod urls"
    This reverts commit 9476613589fdc4e8f74baa1beae0cdfb41a696be.

--- pages/createLog.js
+++ pages/createLog.js
@@ -5,9 +5,6 @@ import { useDisclosure } from '@mantine/hooks';
 import { Modal, Button, Menu } from '@mantine/core';
 import classes from '@styles/createLog.module.css'
 
-const url = process.env.NODE_ENV === 'production' 
-   ? process.env.SITE_URL
-   : ''
   
 export default function Page({ onSignal }) {
     const [isSubmitting, setIsSubmitting] = useState(false)
@@ -27,16 +24,13 @@ export default function Page({ onSignal }) {
     }
 
     async function fetchData() {
-        try {
-            const response = await fetch(`${url}/api/tags`, {
-                method: 'GET'
-            })
-            let data = await response.json(); // properly turns response into array
-            const data2 = data.map(createOptionWrapper);
-            setTags(data2);
-        } catch(e) {
-            console.error(e)
-        }
+        // TODO add a try catch
+        const response = await fetch(`https://animated-hummingbird-6f6978.netlify.app/api/tags`, {
+            method: 'GET'
+        })
+        let data = await response.json(); // properly turns response into array
+        const data2 = data.map(createOptionWrapper);
+        setTags(data2);
     }
 
     let selected = []
--- pages/logs.js
+++ pages/logs.js
@@ -18,11 +18,6 @@ import {
 
 import classes from '../styles/logs.module.css'
 
-const url = process.env.NODE_ENV === 'production' 
-   ? process.env.SITE_URL
-   : ''
-
-
 // this technically works, but ... this data probably shouldn't be publicky cached??
 // export async function getStaticProps(){
 //     const logs = await prisma.Log.findMany();
@@ -51,7 +46,7 @@ export default function Page() {
     async function fetchData() {
         try{
             setIsLoading(true) // Set loading to true when the request starts
-            const response = await fetch(`${url}/api/logs`, {
+            const response = await fetch(`https://animated-hummingbird-6f6978.netlify.app/api/logs`, {
                 method: 'GET'
             })
             let logsArray = await response.json(); // properly turns response into array
@@ -67,7 +62,7 @@ export default function Page() {
         try {
             // https://react.dev/learn/updating-arrays-in-state
             //console.log("deleting log id: ", id);
-            const response = await fetch(`${url}/api/logs/${id}`, {
+            const response = await fetch(`https://animated-hummingbird-6f6978.netlify.app/api/logs/${id}`, {
                 method: 'DELETE'
             })
             if (response.ok){
