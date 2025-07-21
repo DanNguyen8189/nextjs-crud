a11b62616c111e992572e0e877195fec9976d15d
dannguyen8189 <dannguyen8189@gmail.com>
2025-07-21 10:31:25 -0700

    test forcing logs page to be dynamic

--- pages/logs.js
+++ pages/logs.js
@@ -22,7 +22,7 @@ import {
 
 import classes from '../styles/logs.module.css'
 
-//export const dynamic = 'force-dynamic'; // sets this to be a dynamic route (vs static) in netlify build. Needs to be dynamic 
+export const dynamic = 'force-dynamic'; // sets this to be a dynamic route (vs static) in netlify build. Needs to be dynamic 
 // in order to properly avoid problems when one page is doing all fetching during the build time and showing same static page
 // even after data updates
 export const revalidate = 0;
